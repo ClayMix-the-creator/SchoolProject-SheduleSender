e01784a2c1030f8d28f5ea63542b6f52c4099d31
ClayMix <claymix@yandex.ru>
2021-12-09 01:42:50 +0500

    Fixing IndexError mistakes Adding vktoken and community_id changing funcs

--- vk_bot.py
+++ vk_bot.py
@@ -18,7 +18,7 @@ CUR = con.cursor()
 #             'negative_answer': command_negative_answer}
 
 add_person_dict = {'name': '!Подписаться *класс*',
-                   'description': 'подписаться на ежедневную рассылку расписания уроков. Например, !Подписаться 1А',
+                   'description': 'подписаться на ежедневную рассылку расписания уроков. Например, "!Подписаться 1А"',
                    'positive_answer': 'Вы были успешно подписались на рассылку расписания!',
                    'negative_answer': 'Вы уже были записаны на рассылку!'}
 
@@ -32,14 +32,32 @@ command_help_dict = {'name': '!Помощь',
 
 # Extra commands for Admins
 add_class_dict = {'name': '!Добавить класс *класс*',
-                  'description': 'добавить в базу данных таблицу с уроками данного класса. Например, !Добавить класс 1А',
+                  'description': 'добавить в базу данных таблицу с уроками данного класса. Например, "!Добавить класс 1А"',
                   'positive_answer': 'Таблица была успешно добавлена в базу данных',
                   'negative_answer': 'Произошла ошибка при добавлении таблицы в базу данных'}
 
 remove_class_dict = {'name': '!Удалить класс *класс*',
-                     'description': 'убрать из базы данных таблицу с уроками данного класса. Например, !Удалить класс 1А',
+                     'description': 'убрать из базы данных таблицу с уроками данного класса. Например, "!Удалить класс 1А"',
                      'positive_answer': 'Таблица была успешно удалена из базы данных',
                      'negative_answer': 'Произошла ошибка при удалении таблицы из базы данных'}
+
+
+def change_vktoken(token: str) -> bool:
+    request = f"UPDATE settings SET value = '{token}' WHERE [key] = 'vktoken'"
+
+    CUR.execute(request).fetchall()
+    con.commit()
+
+    return True
+
+
+def change_community_id(community_id: str) -> bool:
+    request = f"UPDATE settings SET value = '{community_id}' WHERE [key] = 'community_id'"
+
+    CUR.execute(request).fetchall()
+    con.commit()
+
+    return True
 
 
 def command_help_func() -> str:
@@ -215,7 +233,7 @@ def main():
             user_id = event.obj.message['from_id']
             text = event.obj.message['text'].split()
 
-            if len(text) == 2 and text[0] == add_person_dict['name']:  # add_person_dict, add_person_func
+            if len(text) == 2 and text[0] == add_person_dict['name'].split()[0]:  # add_person_dict, add_person_func
                 status = add_person_func(event.obj.message['from_id'], text[1])
                 console_reply = 'add_person -> '
 
@@ -226,7 +244,7 @@ def main():
                     reply = add_person_dict['negative_answer']
                     console_reply += 'Error'
 
-            elif len(text) == 1 and text[0] == remove_person_dict['name']:  # remove_person_dict, remove_person_func
+            elif len(text) == 1 and text[0] == remove_person_dict['name'].split()[0]:  # remove_person_dict, remove_person_func
                 status = remove_person_func(event.obj.message['from_id'])
                 console_reply = 'remove_person -> '
 
@@ -247,7 +265,7 @@ def main():
 
             elif len(text) == 3 and [text[0], text[1]] == add_class_dict['name'].split()[0:2]:  # add_class_dict, add_class_func
                 if user_id in get_admins():
-                    status = add_class_func(text[3])
+                    status = add_class_func(text[2])
                     console_reply = 'admin.add_class -> '
 
                     if status:
@@ -256,7 +274,7 @@ def main():
                     else:
                         reply = add_class_dict['negative_answer']
                         console_reply += 'Error '
-                    console_reply += f'name: {text[3]}'
+                    console_reply += f'name: {text[2]}'
 
                 else:
                     reply = 'Вы не имеете достаточно прав, для использования данной команды'
@@ -264,16 +282,16 @@ def main():
 
             elif len(text) == 3 and [text[0], text[1]] == remove_class_dict['name'].split()[0:2]:  # remove_class_dict, remove_class_func
                 if user_id in get_admins():
-                    status = remove_class_func(text[3])
+                    status = remove_class_func(text[2])
                     console_reply = 'admin.remove_class -> '
 
                     if status:
-                        reply = remove_person_dict['positive_answer']
+                        reply = remove_class_dict['positive_answer']
                         console_reply += 'OK '
                     else:
                         reply = remove_class_dict['negative_answer']
                         console_reply += 'Error'
-                    console_reply += f'name: {text[3]}'
+                    console_reply += f'name: {text[2]}'
 
                 else:
                     reply = 'Вы не имеете достаточно прав, для использования данной команды'
@@ -290,4 +308,7 @@ def main():
 
 
 if __name__ == '__main__':
+    # change_vktoken()
+    # change_community_id()
+    # add_admin_func()
     main()
