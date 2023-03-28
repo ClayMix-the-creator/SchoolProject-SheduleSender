5f74cb2ffd09437502daad8848c800c4b9e25fcc
ClayMix <claymix007@gmail.com>
2023-03-28 21:15:17 +0500

    Imports update + workable timer

--- timer.py
+++ timer.py
@@ -1,10 +1,16 @@
 # Import built-in python packages
 import sqlite3
+from datetime import datetime
 import time
+import random
 
 # Import downloaded python packages
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+# Import project files
+from vk_bot import get_community_info
+from data.info_updater import refresh_lessons
 
 
 con = sqlite3.connect('db/sendlist.sqlite')
@@ -99,20 +105,66 @@ def get_delay_type() -> int:
         return 1 * 60 * 60 * 24
 
 
+def to_seconds(hours: int=0, minutes: int=0, seconds: int=0) -> int:
+    return hours * 3600 + minutes * 60 + seconds
+
+
+def people_to_send(class_name: str) -> list:
+    request = f'SELECT person_id FROM id_list WHERE person_grade = "{class_name}"'
+    result = [i[0] for i in CUR.execute(request).fetchall()]
+
+    return result
+
+
+def update_text(class_name: str) -> str:
+    text = 'Посмотри, появилось обновление школьного расписания для тебя!\n'
+    request = f'SELECT ALL lessons FROM [{class_name}]'
+    lessons = [i[0] for i in CUR.execute(request)]
+
+    for i in range(len(lessons)):
+        text += f'{i + 1}. {lessons[i]}\n'
+
+    return text
+
+
 def timer():
     """Actual timer of shedule updating. Uses:
     timer_status()
     get_delay()
     """
 
-    print(timer_status())
-    print(timer_enable())
-    print(timer_disable())
+    com_info = get_community_info()
 
-    print(update_delay(1))
-    print(get_delay())
-    print(change_delay_type('SECONDS'))
-    print(get_delay_type())
+    try:
+        vk_session = vk_api.VkApi(token=com_info['token'])
+        longpoll = VkBotLongPoll(vk_session, com_info['community_id'])
+
+        vk = vk_session.get_api()
+    except Exception as e:
+        print(e)
+        return
+
+    while True:
+        now = datetime.now()
+        if timer_status() and (17 <= now.hour < 24 or 0 <= now.hour < 8):
+            classes_to_send = refresh_lessons()
+            print(classes_to_send)
+
+            for i in classes_to_send:
+                for j in people_to_send(i):
+                    vk.messages.send(user_id=j, message=update_text(i),
+                                     # Don't really know, what I should put here, so I put there a random num
+                                     random_id=random.randint(0, 2 ** 64))
+
+        # Means that current time between 8:00 and 17:00
+        # 17:00 is time when usually schedule uploads
+        else:
+            cur_time = to_seconds(hours=now.hour, minutes=now.minute, seconds=now.second)
+            delta = to_seconds(hours=17) - cur_time
+
+            time.sleep(to_seconds(delta))
+
+        time.sleep(get_delay())
 
 
 if __name__ == '__main__':
